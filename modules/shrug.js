4922cca427b4a004168a576b27950b609d14a09d
torsina <theo.sagot.1@gmail.com>
2017-05-18 01:32:00 +0200

    debug of shrug

--- modules/shrug.js
+++ modules/shrug.js
@@ -1,6 +1,6 @@
 const perms = require('../config/perm/perms');
 module.exports = {
-    help: "¯\_(ツ)_/¯",
+    help: "\u00af\\_(\u30c4)_\/\u00af",
     func: (client, msg, args, role) => {
         if(perms.check("interaction.shrug.base", role, msg.author.id) !== true) {
             msg.channel.sendMessage("You don't have the permission `interaction.shrug.base`");
