e1cd4ac2180bc011b9a5a3df99f89fdabbfab0c6
torsina <theo.sagot.1@gmail.com>
2017-06-13 17:34:29 +0200

    doc added guild property added to the schemas optimization in getAnalyticByDate

--- util/db.js
+++ util/db.js
@@ -3,17 +3,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const db = module.exports = {};
 
+let serverSchema = new Schema({
+    id: String,
+    config: {
+        prefix: String,
+        registeredRoles: Array,
+        perm: Object,
+        report: Number,
+        channel: {
+            log: String,
+            nsfw: Array //of object
+        },
+        mod: {
+            purge: {
+                safe: Boolean,
+                value: Number
+            }
+        }
+    }
+});
+
 let imagePostSchema = new Schema({
     id_image: String,
     id_message: String,
     id_file: String,
+    channel: String,
+    guild: String,
     report_count: Number
 });
 
 let reposterSchema = new Schema({
     id: String,
     enter: String,
-    exit: String
+    exit: String,
+    guild: String,
+    role: String
 });
 
 let userSchema = new Schema({
@@ -23,6 +47,7 @@ let userSchema = new Schema({
 });
 
 let eventSchema = new Schema({
+    guild: String,
     name: String,
     creator: String,
     desc: String,
@@ -47,7 +72,9 @@ let imagePost = mongoose.model('imagePost', imagePostSchema);
 let reposter = mongoose.model('reposter', reposterSchema);
 let event = mongoose.model('event', eventSchema);
 let analytic = mongoose.model('analytic', analyticSchema);
-
+/**
+ * create the connection with the database
+ */
 db.load = () => {
     mongoose.connect("mongodb://192.168.1.30/arys"); //id_image, id_message, id_file, report_count
     let db = mongoose.connection;
@@ -56,17 +83,30 @@ db.load = () => {
         console.log("connected to mongodb server");
     });
 };
-
+/**
+ * get the date
+ * @returns {string}
+ */
 function date() {
     let timestamp = new Date();
     return timestamp.getFullYear() + '-' + (timestamp.getMonth() + 1) + '-' + timestamp.getDate() + ' ' + timestamp.getHours() + ':' + timestamp.getMinutes() + ":" + timestamp.getSeconds();
 }
-
-db.createPost = (id_image, id_message, id_file) => {
+/**
+ * create a new post in the collection
+ * @param {String} image - the line in the file + 1
+ * @param {String} message - id of the message of the post
+ * @param {String} file - which file does this post come from
+ * @param {String} channel - channel id where this is posted
+ * @param {String} guild - guild id where this is posted
+ * @returns {Promise}
+ */
+db.createPost = (image, message, file, channel, guild) => {
     let query = new imagePost({
-        id_image: id_image,
-        id_message: id_message,
-        id_file: id_file,
+        id_image: image,
+        id_message: message,
+        id_file: file,
+        channel: channel,
+        guild: guild,
         report_count: 0
     });
     return new Promise((resolve, reject) => {
@@ -76,11 +116,18 @@ db.createPost = (id_image, id_message, id_file) => {
         }).catch(reject);
     });
 };
-
-db.getPost = (id_image, id_file) => {
+/**
+ * get a precise post from the list
+ * @param {String} image - the line in the file + 1
+ * @param {String} file - which file does this post come from
+ * @param {String} guild - guild id where this is posted
+ * @returns {Promise}
+ */
+db.getPost = (image, file, guild) => {
     let query  = imagePost.where({
-        id_image: id_image,
-        id_file: id_file
+        id_image: image,
+        id_file: file,
+        guild: guild
     });
     return new Promise((resolve, reject) => {
         query.findOne().then(doc => {
@@ -92,14 +139,21 @@ db.getPost = (id_image, id_file) => {
     })
 
 };
-
-db.getAllPost = (image, message, file) => {
+/**
+ * get the post entries with the defined parameters
+ * @param {Number} image - the line in the file + 1
+ * @param {String} message - id of the message of the post
+ * @param {String} file - which file does this post come from
+ * @param {String} guild - guild id where this is posted
+ * @returns {Promise}
+ */
+db.getAllPost = (image, message, file, guild) => {
     let searchobj = {};
     if(image !== undefined) searchobj.id_image = image;
     if(message !== undefined) searchobj.id_message = message;
     if(file !== undefined) searchobj.id_file = file;
+    searchobj.guild = guild;
     let query = imagePost.where(searchobj);
-
     return new Promise((resolve, reject) => {
         query.find().then(doc => {
             if (doc === null) return reject(new Error('no entry found'));
@@ -107,10 +161,16 @@ db.getAllPost = (image, message, file) => {
         }).catch(reject);
     });
 };
-
-db.reportPost = (id_message) => {
+/**
+ * add 1 to the report count of the post
+ * @param {String} message - id of the message of the post
+ * @param {String} guild - guild id where this is posted
+ * @returns {Promise}
+ */
+db.reportPost = (message, guild) => {
     let query  = imagePost.where({
-        id_message: id_message
+        id_message: message,
+        guild: guild
     });
     return new Promise((resolve, reject) => {
         query.findOne(function (err, doc) {
@@ -121,10 +181,16 @@ db.reportPost = (id_message) => {
         }).catch(reject);
     });
 };
-
-db.deletePost = (id_message) => {
+/**
+ * delete the entry from the post collection
+ * @param {String} message - id of the message of the post
+ * @param {String} guild - guild id where this is posted
+ * @returns {Promise}
+ */
+db.deletePost = (message, guild) => {
     let query  = imagePost.where({
-        id_message
+        id_message: message,
+        guild: guild
     });
     return new Promise((resolve, reject) => {
         query.findOneAndRemove.then(doc => { //error, no entry found to delete
@@ -133,10 +199,16 @@ db.deletePost = (id_message) => {
         }).catch(reject);
     });
 };
-
-db.createReposter = (member) => {
+/**
+ * add a new entry in the reposter collection
+ * @param {String} member - id of the user being logged
+ * @param {String} guild - guild id where this is posted
+ * @returns {Promise}
+ */
+db.createReposter = (member, guild) => {
     let query = new reposter({
         id: member,
+        guild: guild,
         enter: date()
     });
     return new Promise((resolve, reject) => {
@@ -146,10 +218,16 @@ db.createReposter = (member) => {
         }).catch(reject);
     });
 };
-
-db.endReposter = (id) => {
+/**
+ * add the end date to the reposter entry
+ * @param {String} member - id of the user being logged
+ * @param {String} guild - guild id where this is posted
+ * @returns {Promise}
+ */
+db.endReposter = (member, guild) => {
     let query  = reposter.where({
-        id: id
+        id: member,
+        guild: guild
     });
     return new Promise((resolve, reject) => {
         query.find(function (err, doc) {
@@ -169,13 +247,21 @@ db.endReposter = (id) => {
         });
     });
 };
-
-db.getReposter = (member) => {
+/**
+ * get the reposter entries with the defined parameters
+ * @param guild - mandatory, guild id where this is posted
+ * @param member - id of the user being logged
+ * @returns {Promise}
+ */
+db.getReposter = (guild, member) => {
     if(member !== undefined) {
         let query = reposter.where({
-            id: member
+            id: member,
+            guild: guild
         });
+
         return new Promise((resolve, reject) => {
+            if(guild === undefined) return reject(new Error('no guild scope was used'));
             query.find().then(doc => {
                 if (Object.keys(doc).length === 0) {
                     return reject(new Error('no entry found'));
@@ -185,7 +271,11 @@ db.getReposter = (member) => {
         });
     } else {
         return new Promise((resolve, reject) => {
-            reposter.find().then(doc => {
+            if(guild === undefined) return reject(new Error('no guild scope was used'));
+            let query = reposter.where({
+                guild: guild
+            });
+            query.find().then(doc => {
                 if (Object.keys(doc).length === 0) {
                     return reject(new Error('no entry found'));
                 }
@@ -194,9 +284,12 @@ db.getReposter = (member) => {
         });
     }
 };
-
+/**
+ * delete the reposter collection
+ * @returns {*}
+ */
 db.deleteReposter = () => {
-    reposter.collection.drop();
+    return reposter.collection.drop();
 };
 
 /*
@@ -333,7 +426,14 @@ db.editEventScheduleIrregular = (name, date) => {
     });
 };
 */
-
+/**
+ * create a new analytic in the collection
+ * @param {String} item - full id of the emote being registered
+ * @param {String} member - id of the member who used the emote
+ * @param {String} channel - id of the channel where emote was used
+ * @param {String} guild - id of the guild where emote was used
+ * @returns {Promise}
+ */
 db.createAnalytic = (item, member, channel, guild) => { //
     let query = new analytic({
         item: item,
@@ -349,19 +449,30 @@ db.createAnalytic = (item, member, channel, guild) => { //
         }).catch(reject);
     });
 };
-
-db.getAnalytic = (name) => { //
+/**
+ * get the analytic entries with the defined parameters
+ * @param {String} item - full id of the emote being registered
+ * @param {String} guild - id of the guild where you want to search
+ * @returns {Promise}
+ */
+db.getAnalytic = (item, guild) => { //
     let query = analytic.where({
-        item: name
+        item: item,
+        guild: guild
     });
     return new Promise((resolve, reject) => {
+        if(guild === undefined) return reject(new Error('no guild scope was used'));
         query.find().then(doc => {
             if (doc === null) return reject(new Error('no entry found'));
             resolve(doc);
         }).catch(reject);
     });
 };
-
+/**
+ *
+ * @param {Object} obj
+ * @returns {Promise}
+ */
 db.getDifferentAnalytic = (obj) => { //
     return new Promise((resolve, reject) => {
         obj.find().distinct('item').then(doc => {
@@ -370,9 +481,13 @@ db.getDifferentAnalytic = (obj) => { //
         }).catch(reject);
     });
 };
-
+/**
+ *
+ * @param {String} name
+ * @param {Object} obj
+ * @returns {Promise}
+ */
 db.countAnalyticByName = (name, obj) => { //
-    //console.log(obj);
     return new Promise((resolve, reject) => {
         obj.where({item: name}).find().then(doc => {
             if (doc === null) return reject(new Error('no entry found'));
@@ -381,10 +496,15 @@ db.countAnalyticByName = (name, obj) => { //
         }).catch(reject);
     });
 };
-
-db.countAnalytic = (Object) => { //main func
+/**
+ * get an array with the emojis and the number of time they were used
+ * @param {String} guild - id of the guild where you want to search
+ * @param {Object} Object - object of the query used for specific time range
+ * @returns {Promise}
+ */
+db.countAnalytic = (guild, Object) => { //main func
     let obj;
-    if (Object === undefined) obj = analytic;
+    if (Object === undefined) obj = analytic.where({guild: guild});
     else obj = Object;
     let stack = [];
     return new Promise((resolve, reject) => {
@@ -399,8 +519,14 @@ db.countAnalytic = (Object) => { //main func
         }).catch(console.error);
     });
 };
-
-db.getAnalyticInDate = (min, max) => { //set timeframe
+/**
+ * get an array with the emojis and the number of time they were used inside of a time range
+ * @param {String} min - starting point of the time range
+ * @param {String} max - ending point of the time range
+ * @param {String} guild - id of the guild where you want to search
+ * @returns {Promise}
+ */
+db.getAnalyticInDate = (min, max, guild) => { //set timeframe
     let minDate, maxDate;
     if (min === undefined) minDate = 0;
     else minDate = min;
@@ -409,16 +535,22 @@ db.getAnalyticInDate = (min, max) => { //set timeframe
     let minDateObj = new Date(minDate);
     let maxDateObj = new Date(maxDate);
     console.log("triggered");
-    let query = analytic.where('date').lt(maxDateObj.getTime()).gt(minDateObj.getTime());
+    let query = analytic.where('date').lt(maxDateObj.getTime()).gt(minDateObj.getTime()).where({guild: guild});
     return new Promise((resolve, reject) => {
         query.find().then(doc => {
             if (doc === null) return console.log("error : " + min + " " + max); //reject(new Error('no entry for this period'))
-            db.countAnalytic(query).then(obj => resolve(obj)).catch(console.error);
+            db.countAnalytic(guild, query).then(obj => resolve(obj)).catch(console.error);
         }).catch(console.error);
     });
 };
-
-db.getAnalyticByDate = (min, max) => {
+/**
+ * get an array of object with the emojis and the number of time they were used for each day of the time range
+ * @param {String} min - starting point of the time range
+ * @param {String} max - ending point of the time range
+ * @param {String} guild - id of the guild where you want to search
+ * @returns {Promise}
+ */
+db.getAnalyticByDate = (min, max, guild) => {
     let minDate, maxDate;
     minDate = min || 0;
     maxDate = max || Date.now();
@@ -427,18 +559,22 @@ db.getAnalyticByDate = (min, max) => {
     let days = Math.floor((b-a)/86400000); //number of day
     let stack = [];
     return new Promise((resolve) => {
-        let query = analytic.where('date').gt(a).lt(b);
+        let query = analytic.where('date').gt(a).lt(b).where({guild: guild});
         query.find().then(doc => {
-            for(i=0; i<days;i++) { //for each day
+            for(let i=0; i<days;i++) { //for each day
                 let start = a + i*86400000; //a =
                 let end = start + 86400000;
                 stack[i] = {};
-                let subDoc = doc.filter(function (o) {return (parseInt(o.date) >= parseInt(start) && parseInt(o.date) <= parseInt(end))});
+
+                let subDoc = doc.filter(function (o) {
+                    let date = parseInt(o.date);
+                    return (date >= parseInt(start) && date <= parseInt(end));
+                });
                 //console.log(subDoc.length + "max : " + doc.length +", start : "+ new Date(start) + " , stop : " + new Date(end) + " , i: " + i);
                 let nameStack = [...new Set(subDoc.map(analytic => analytic.item))];
                 let subStack = [];
                 if(nameStack.length !== 0) {
-                    for(j=0;j<nameStack.length;j++) {
+                    for(let j=0;j<nameStack.length;j++) {
                         let name = nameStack[j];
                         let count = subDoc.filter(function (o) {
                             return (o.item === name);
