e7704dba449af0901e15ef0ba6f637f1834df99f
torsina <theo.sagot.1@gmail.com>
2017-06-20 23:24:07 +0200

    add of emoji deletion minor changes swap to rethinkdbdash 2/?

--- Arys.js
+++ Arys.js
@@ -3,14 +3,15 @@ const fs = require('fs');
 const Client = new Discord.Client();
 const config = require('./config/config');
 const roles = require('./config/perm/roles');
-const db = require('./util/db');
+const db = require('./util/rethinkdb');
+const mongo = require('./util/db');
 const web = require('./web/server');
 Client.login(config.discord.token.bot).catch(console.error);
 Client.once('ready', () => {
     console.time('loading');
     Client.load();
     roles.load();
-    db.load();
+    mongo.load();
     Client.user.setGame('type $help');
     console.timeEnd('loading');
     console.log('I am ready!');
@@ -40,10 +41,10 @@ Client.load = (command) => {
 
 Client.on('guildMemberUpdate', (oldMember, newMember) => {
     if (!oldMember.roles.has(config.reposter) && newMember.roles.has(config.reposter)) {
-        db.createReposter(oldMember.id);
+        db.createListenedRole(oldMember.guild.id, config.reposter, oldMember.id).catch(console.error);
     }
     if(oldMember.roles.has(config.reposter) && !newMember.roles.has(config.reposter)) {
-        db.endReposter(oldMember.id);
+        db.endListenedRole(oldMember.guild.id, config.reposter, oldMember.id).catch(console.error);
     }
 });
 
@@ -66,19 +67,25 @@ Client.on('message', message => {
         message.channel.sendMessage("throw him in a pit and let me do the rest")
     }
     //emoji delete system
-    if (isEmoji(message.content) === true) {
-            console.log("there is an emoji here");
+    if (isEmoji(message.content) === true && message.channel.id !== "249626680434491392") {
+            console.log("there is an emoji here : " + message.channel.name + " ,by : " + message.author.username + '#' + message.author.discriminator);
+            message.delete();
+            message.reply("***GTFO RETARD AND READ THE RULES IN *** <#242655328410402816> <:feelsrageman:246603943768096769>").then(m => {
+                setTimeout(function() {
+                    m.delete();
+                }, 7000);
+            });
     }
-    //server emoji analytics
+    //server emote analytics
     if(!message.author.bot && config.env !== "dev") {
-        let serverEmojis = message.guild.emojis.array();
-        let emojiStack = message.content.match(/<:(\w+):(\d+)>/g);
-        if (emojiStack !== null) {
-            for (let i = 0; i<serverEmojis.length; i++) {
-                let emoji = "<:" + serverEmojis[i].name + ":" + serverEmojis[i].id + ">";
-                if (emojiStack.includes(emoji)) {
-                    db.createAnalytic(emoji, message.author.id, message.channel.id, message.guild.id).catch(console.error);
-                    console.log(emoji);
+        let serverEmotes = message.guild.emojis.array();
+        let emoteStack = message.content.match(/<:(\w+):(\d+)>/g);
+        if (emoteStack !== null) {
+            for (let i = 0; i<serverEmotes.length; i++) {
+                let emote = "<:" + serverEmotes[i].name + ":" + serverEmotes[i].id + ">";
+                if (emoteStack.includes(emote)) {
+                    db.createAnalytic(message.guild.id, message.channel.id, emote, message.author.id).catch(console.error);
+                    console.log(emote);
                 }
             }
         }
@@ -90,7 +97,7 @@ Client.on('message', message => {
             let member = message.guild.member(Client.users.get(message.author.id));
             let role = check(member);
             let guild = message.guild;
-
+            let args = message.content.split(' ');
             let command = args[0].slice(config.discord.prefix.length);
             args.splice(0, 1);
 
@@ -143,12 +150,12 @@ function check(member){
 }
 
 function isEmoji(str) {
+    //'\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
+    //'\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F // U+1F680 to U+1F6FF // U+263A // 1F600 - 1F636 // U+1F621
     let ranges = [
-        //'\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
-        //'\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F
-        '\ud83d[\ude00-\udeff]' // U+1F680 to U+1F6FF // U+263A // 1F600 - 1F636
+        '\ud83d[\ude00-\udeff]'
     ];
-    if (str.match(ranges.join('|'))) {
+    if (str.match(ranges.join('|')) && !str.match('\u{1F621}')) {
         return true;
     } else {
         return false;
