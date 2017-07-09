e005865b0673f378655d8d614ef399fc8326e463
torsina <theo.sagot.1@gmail.com>
2017-07-10 01:56:26 +0200

    add of sell fix of buy

--- modules/sell.js
+++ modules/sell.js
@@ -2,12 +2,12 @@ const perms = require('../config/perm/perms');
 const config = require('../config/config');
 const db = require('../util/rethinkdb');
 const money = require('../util/money');
+
 module.exports = {
-    help: 'Ping, Pong',
+    help: 'haxxed',
     func: async (client, msg, args, role) => {
-        //if(config.env === "dev") return;
         if(!args[0]) {
-            return msg.channel.send("Please use this command like the following:\n`$buy <category> <item>`")
+            return msg.channel.send("Please use this command like the following:\n`$sell <category> <item>`")
         } else {
             let array = [];
             let list = await db.getShopsCategory(msg.guild.id);
@@ -32,12 +32,12 @@ module.exports = {
                     let category = await db.getShopsCategory(msg.guild.id, args[0]).catch(console.error);
                     switch(category.type) {
                         case "role":
-                            if(msg.guild.members.get(msg.author.id).roles.find("name", item.item)) {
-                                return msg.channel.send("You already have that role.")
+                            if(!msg.guild.members.get(msg.author.id).roles.find("name", item.item)) {
+                                return msg.channel.send("You don't have that role to sell.")
                             }
                             if(msg.guild.members.get(client.user.id).hasPermission('MANAGE_ROLES')) {
-                                await db.changeMoney(msg.guild.id, msg.author.id, -item.price).catch(e => {msg.channel.send(e.message)});
-                                msg.guild.members.get(msg.author.id).addRole(msg.guild.roles.find("name", item.item)).catch(console.error);
+                                msg.guild.members.get(msg.author.id).removeRole(msg.guild.roles.find("name", item.item)).catch(console.error);
+                                return await db.changeMoney(msg.guild.id, msg.author.id, item.price/2).catch(e => {msg.channel.send(e.message)});
                             } else {
                                 return msg.channel.send("I don't have the permission to do that D:")
                             }
