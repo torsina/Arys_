ba15068cdef9bc8e052125684018dd190a66ae1c
torsina <theo.sagot.1@gmail.com>
2017-07-16 21:23:45 +0200

    give command now display a message when sucessfull

--- modules/give.js
+++ modules/give.js
@@ -2,6 +2,7 @@ const perms = require('../util/perm');
 const config = require('../config/config');
 const db = require('../util/rethinkdb');
 const money = require('../util/money');
+const Discord = require('discord.js');
 
 const bitField = {
     help: 1 << 0,
@@ -12,12 +13,14 @@ const bitField = {
 module.exports = {
     help: 'haxxed',
     func: async (client, msg, args, guildMember) => {
+        let name = await money.getName(msg.guild.id);
         if(!msg.mentions.users) {
             try{await perms.check(guildMember, "give.self")}catch(e) {return msg.channel.send(e.message)}
-            msg.channel.send("You gave yourself " + args[0]);
+            msg.channel.send("You gave yourself " + args[0] + " " + name);
             db.changeMoney(msg.guild.id, msg.author.id, args[0]).catch(console.error);
         } else {
             try{await perms.check(guildMember, "give.other")}catch(e) {return msg.channel.send(e.message)}
+            msg.channel.send("You gave " + args[0] + " " + name + " to " + msg.mentions.users.first().toString());
             db.changeMoney(msg.guild.id, msg.mentions.users.first().id, args[args.length-1]).catch(console.error);
         }
     }
