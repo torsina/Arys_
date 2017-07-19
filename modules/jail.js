de602988e3d64c0e5fc91ae04942baeb48257121
torsina <theo.sagot.1@gmail.com>
2017-07-19 14:00:01 +0200

    remove option of jail added + guild permission check

--- modules/jail.js
+++ modules/jail.js
@@ -1,22 +1,24 @@
 const perms = require('../util/perm');
 const db = require('../util/rethinkdb');
-const money = require('../util/money');
 const config = require('../config/config');
 const Discord = require('discord.js');
 
 const bitField = {
     help: 1 << 0,
-    set: 1 << 1
+    change: 1 << 1
 };
 
 module.exports = {
-    help: 'Get lucky, or not.',
+    help: "Do you think I'm not watching?",
     func: async (client, msg, args, guildMember) => {
         //if(config.env === "dev") return;
         try {
             await perms.check(guildMember, "jail.set")
         } catch (e) {
             return msg.channel.send(e.message)
+        }
+        if(!msg.guild.members.get(client.user.id).permissions.has("MANAGE_ROLES")) {
+            return msg.channel.send("I can't start this feature because I don't have the Role manager permission.")
         }
         let embed = new Discord.RichEmbed()
             .setTimestamp()
@@ -25,14 +27,27 @@ module.exports = {
             case "-set": {
                 if (msg.mentions.roles.first()) {
                     try {
-                        await db.createListenedRole(msg.guild.id, msg.mentions.roles.first().id)
+                        await db.createListenedRole(msg.guild.id, msg.mentions.roles.first().id);
                     } catch (e) {
                         embed.setDescription(e.message);
                         return msg.channel.send({embed})
                     }
                     embed.setDescription("the role " + msg.mentions.roles.first().name + " was added to the watch list");
                     msg.channel.send({embed});
-                }
+                } else return msg.channel.send("Please tag the role you want to add to the watch list.");
+                break;
+            }
+            case "-remove": {
+                if (msg.mentions.roles.first()) {
+                    try {
+                        await db.deleteListenedRole(msg.guild.id, msg.mentions.roles.first().id);
+                    } catch (e) {
+                        embed.setDescription(e.message);
+                        return msg.channel.send({embed})
+                    }
+                    embed.setDescription("the role " + msg.mentions.roles.first().name + " was removed from the watch list");
+                    msg.channel.send({embed});
+                } else return msg.channel.send("Please tag the role you want to add to the watch list.");
                 break;
             }
             case "-list": {
