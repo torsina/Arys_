0a0fc6e76d78c89c8e38ff8b77a4937437aebad2
torsina <theo.sagot.1@gmail.com>
2017-08-02 16:40:11 +0200

    -see argument on credits is merged with base command

--- modules/credits.js
+++ modules/credits.js
@@ -24,29 +24,41 @@ module.exports = {
                 .setTimestamp()
                 .setDescription(`<@${msg.author.id}>,You have ${_money.amount} ${moneyName}`);
             msg.channel.send({embed});
-        }else if (msg.mentions.users.first() !== msg.author && args[1] === "-see"){
-            try{await perms.check(guildMember, msg.channel.id, "credits.other")}catch(e) {return msg.channel.send(e.message)}
-            let moneyName = await money.getName(msg.guild.id);
-            let _money = await money.get(msg.mentions.users.first().id, msg.guild.id);
-            let embed = new Discord.RichEmbed()
-                .setColor(0x00AE86)
-                .setFooter('asked by ' + msg.author.tag)
-                .setTimestamp()
-                .setDescription(`${msg.mentions.users.first()} has ${_money.amount} ${moneyName}`);
-            msg.channel.send({embed});
         } else if(msg.mentions.users.first() !== msg.author){
-            if(parseInt(args[args.length-1]) < 0) return msg.channel.send("You can't send negative amounts");
-            try{await perms.check(guildMember, msg.channel.id, "credits.other")}catch(e) {return msg.channel.send(e.message)}
-            let moneyName = await money.getName(msg.guild.id);
-            let _money = await money.get(msg.author.id, msg.guild.id);
-            let embed = new Discord.RichEmbed()
-                .setColor(0x00AE86)
-                .setFooter('asked by ' + msg.author.tag)
-                .setTimestamp()
-                .setDescription(`${msg.author.toString()}, You transferred ${args[args.length-1]} ${moneyName} to ${msg.mentions.users.first().toString()}`);
-            await db.changeMoney(msg.guild.id, msg.author.id, -parseInt(args[args.length-1])).catch(e => {return msg.channel.send(e.message)});
-            await db.changeMoney(msg.guild.id, msg.mentions.users.first().id, parseInt(args[args.length-1])).catch(e => {return msg.channel.send(e.message)});
-            msg.channel.send({embed});
+            if(args.length > 1) {
+                if(parseInt(args[args.length-1]) < 0) return msg.channel.send("You can't send negative amounts");
+                try{await perms.check(guildMember, msg.channel.id, "credits.other")}catch(e) {return msg.channel.send(e.message)}
+                let moneyName = await money.getName(msg.guild.id);
+                let _money = await money.get(msg.author.id, msg.guild.id);
+                let embed = new Discord.RichEmbed()
+                    .setColor(0x00AE86)
+                    .setFooter('asked by ' + msg.author.tag)
+                    .setTimestamp()
+                    .setDescription(`${msg.author.toString()}, You transferred ${args[args.length-1]} ${moneyName} to ${msg.mentions.users.first().toString()}`);
+                try {
+                    await db.changeMoney(msg.guild.id, msg.author.id, -parseInt(args[args.length-1]));
+                }
+                catch(e) {
+                    return msg.channel.send(e.message);
+                }
+                try {
+                    await db.changeMoney(msg.guild.id, msg.mentions.users.first().id, parseInt(args[args.length-1]));
+                }
+                catch(e) {
+                    return msg.channel.send(e.message);
+                }
+                msg.channel.send({embed});
+            } else {
+                try{await perms.check(guildMember, msg.channel.id, "credits.other")}catch(e) {return msg.channel.send(e.message)}
+                let moneyName = await money.getName(msg.guild.id);
+                let _money = await money.get(msg.mentions.users.first().id, msg.guild.id);
+                let embed = new Discord.RichEmbed()
+                    .setColor(0x00AE86)
+                    .setFooter('asked by ' + msg.author.tag)
+                    .setTimestamp()
+                    .setDescription(`${msg.mentions.users.first()} has ${_money.amount} ${moneyName}`);
+                msg.channel.send({embed});
+            }
         } else {
             return msg.channel.send("You can't give money to yourself!")
         }
