7032b6a8ddd04069ca9337b078abdbb47e4ef913
torsina <theo.sagot.1@gmail.com>
2017-08-07 16:29:38 +0200

    add of remove argument for giveme

--- modules/giveme.js
+++ modules/giveme.js
@@ -4,11 +4,12 @@ const db = require('../util/rethinkdb');
 const Discord = require('discord.js');
 
 const bitField = {
-    help: 1 << 0,
+    help: 1 << 0, //help visibility
     set: 1 << 1, //add a role to the user
+    remove: 1 << 5, //remove a role from a user
     list: 1 << 2, //give the list of the giveme roles
     add: 1 << 3, //add a role to the list
-    delete: 1 << 4
+    delete: 1 << 4 //delete a role from the list
 };
 
 module.exports = {
@@ -23,7 +24,7 @@ module.exports = {
                 let nameInput = array.join(" ");
                 nameInput = nameInput.substr(1);
                 let role = msg.guild.roles.find("name", nameInput);
-                if(role) {
+                if(role && !msg.guild.members.get(msg.author.id).roles.get(role.id)) {
                     let doc = await db.getAutoRole(msg.guild.id, role.id);
                     if(doc) {
                         msg.guild.members.get(msg.author.id).addRole(role.id).catch(console.error);
@@ -41,9 +42,57 @@ module.exports = {
                             .setTimestamp();
                         return msg.channel.send({embed});
                     }
+                } else if(msg.guild.members.get(msg.author.id).roles.get(role.id)){
+                    let embed = new Discord.RichEmbed()
+                        .setDescription("You already have this role")
+                        .setColor("RED")
+                        .setFooter('asked by ' + msg.author.tag)
+                        .setTimestamp();
+                    return msg.channel.send({embed});
                 } else {
                     let embed = new Discord.RichEmbed()
-                        .setDescription("The role you asked was not found, please check the role list with \n`$giveme -list` TEST")
+                        .setDescription("The role you asked was not found, please check the role list with \n`$giveme -list`")
+                        .setColor("RED")
+                        .setFooter('asked by ' + msg.author.tag)
+                        .setTimestamp();
+                    return msg.channel.send({embed});
+                }
+            }
+            case "-remove": {
+                try{await perms.check(guildMember, msg.channel.id, "giveme.remove")}catch(e) {return msg.channel.send(e.message)}
+                let array = args.slice();
+                delete array[0];
+                let nameInput = array.join(" ");
+                nameInput = nameInput.substr(1);
+                let role = msg.guild.roles.find("name", nameInput);
+                if(role && msg.guild.members.get(msg.author.id).roles.get(role.id)) {
+                    let doc = await db.getAutoRole(msg.guild.id, role.id);
+                    if(doc) {
+                        msg.guild.members.get(msg.author.id).removeRole(role.id).catch(console.error);
+                        let embed = new Discord.RichEmbed()
+                            .setDescription("I removed you the role " + role.name)
+                            .setColor("BLUE")
+                            .setFooter('asked by ' + msg.author.tag)
+                            .setTimestamp();
+                        return msg.channel.send({embed});
+                    } else {
+                        let embed = new Discord.RichEmbed()
+                            .setDescription("The role you asked was not found in my list, please check the role list with \n`$giveme -list`")
+                            .setColor("RED")
+                            .setFooter('asked by ' + msg.author.tag)
+                            .setTimestamp();
+                        return msg.channel.send({embed});
+                    }
+                } else if(!msg.guild.members.get(msg.author.id).roles.get(role.id)){
+                    let embed = new Discord.RichEmbed()
+                        .setDescription("You have no role to remove")
+                        .setColor("RED")
+                        .setFooter('asked by ' + msg.author.tag)
+                        .setTimestamp();
+                    return msg.channel.send({embed});
+                } else {
+                    let embed = new Discord.RichEmbed()
+                        .setDescription("You have no role to remove")
                         .setColor("RED")
                         .setFooter('asked by ' + msg.author.tag)
                         .setTimestamp();
@@ -75,12 +124,14 @@ module.exports = {
                 nameInput = nameInput.substr(1);
                 let role = msg.guild.roles.find("name", nameInput);
                 if(role) {
-                    await db.createAutoRole(msg.guild.id, role.id);
                     let embed = new Discord.RichEmbed()
                         .setDescription("I added the role " + role.name)
                         .setColor("BLUE")
                         .setFooter('asked by ' + msg.author.tag)
                         .setTimestamp();
+                    await db.createAutoRole(msg.guild.id, role.id).catch(e => {
+                        embed.setDescription(e.message);
+                    });
                     return msg.channel.send({embed});
                 } else {
                     let embed = new Discord.RichEmbed()
@@ -116,6 +167,9 @@ module.exports = {
                         .setTimestamp();
                     return msg.channel.send({embed});
                 }
+            }
+            case undefined: {
+                return msg.channel.send("giveme use: \n`$giveme -list` to get the role list\n`$giveme -set <role>` to get a role\n`$giveme -remove <role>` to remove yourself a role");
             }
         }
     }
