fae341d7bff82ba0a98ff311270a5f65c9f2db8d
torsina <theo.sagot.1@gmail.com>
2018-05-30 11:15:48 +0200

    progress and testings

--- src/web/script.js
+++ src/web/script.js
@@ -11,57 +11,15 @@ app.config(function($routeProvider) {
             controller  : 'mainController'
         })
 
-        // route for the about page
-        .when('/about', {
-            templateUrl : 'pages/home.html',
-            controller  : 'aboutController'
-        })
-
-        // route for the contact page
-        .when('/contact', {
-            templateUrl : 'pages/home.html',
-            controller  : 'contactController'
-        })
         .when('/servers',{
             templateUrl : 'pages/servers.html' ,
             controller : 'serversController'
         })
 
-        .when("/test", {
-            templateUrl: "pages/test.html",
-            controller: "testController"
-        })
-
         .when('/server',{
             templateUrl : 'pages/server.html' ,
-            controller : 'serverController',
-            resolve: {
-                api: function() {
-                    var xhttp = new XMLHttpRequest();
-                    xhttp.open("GET", "http://localhost:5000/API/servers");
-                    xhttp.setRequestHeader("Content-type", "application/json");
-                    xhttp.send();
-                    xhttp.onload = function (data) {
-                        if (xhttp.readyState === 4) {
-                            if (xhttp.status === 200) {
-                                var response = xhttp.responseText;
-                                return response;
-                                // ici, response est un tableau
-                                // chaque élément du tableau sera un serveur
-                                // chaque index de ce tableau sera un objet
-                                // chaque objet aura les propriétés : guildID, iconURL, guildName
-                            } else {
-                                console.error(xhttp.statusText);
-                            }
-                        }
-                    };
-                }
-            }
+            controller : 'serverController'
         });
-});
-
-app.controller("testController", (api) => {
-    $scope.servers = api;
 });
 
 // create the controller and inject Angular's $scope
@@ -70,27 +28,22 @@ app.controller('mainController', function($scope) {
     $scope.message = 'Everyone come and see how good I look!';
 });
 
-app.controller('aboutController', function($scope) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://lovalhosy:5000/API/", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send();
-    var response = JSON.parse(xhttp.responseText);
-    $scope.message = 'Look! I am an about page.';
-});
-
-app.controller('contactController', function($scope) {
-    $scope.message = 'Contact us! JK. This is just a demo.';
-});
-
 app.controller('serverController', function($scope) {
     $scope.money = '200';
 });
 
 
-app.controller('serversController', function($scope) {
-    $scope.money = '200';
+app.controller('serversController', function($scope, $http) {
+    $http.get('/api/servers').then((data) => {
+        const response = data.data;
+        $scope.$apply(() => {
+            console.log(response[0].guildID);
+            $scope.message = response[0].guildID;
+        });
+    }, (err) => console.error(err));
+    //$scope.money = '200';
 });
+
 
 /*
 
